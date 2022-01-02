77b2560111f17d31d9afac58325c6f4956ed1476
Robbi-Blechdose <robbi.blechdose@posteo.de>
2022-01-02 13:14:00 +0100

    Moved icon to res folder

--- package.sh
+++ package.sh
@@ -6,7 +6,6 @@ make TARGET=funkey
 mkdir -p opk
 cp  main-funkey.elf opk/main-funkey.elf
 cp -r ./res opk/res
-cp icon.png opk/icon.png
 cp TriSpace.funkey-s.desktop opk/TriSpace.funkey-s.desktop
 
 mksquashfs ./opk TriSpace.opk -all-root -noappend -no-exports -no-xattrs
